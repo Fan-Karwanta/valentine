59737555c100c32053b91bc0867e70b1e10d2c07
Fan Karwanta <135393498+Fan-Karwanta@users.noreply.github.com>
2024-02-11 12:32:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yeyyyyy Tiwahoooon Nako Ikaw Da taman tamannn <3 ";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Are Sureee da sure?",
+    "Bad man ka oi :(",
+    "AYAAAWWWW BAAAAAAAA:(",
+    "Unsa man ka oi dili naman ko nimo LAB DA LAB",
+    "Sige Judddd tuploka jud ng NO juuuudddd awa ron ",
+    "HMPPP! Bahala naka oi ma sad sad na imo da PON-PON :(",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
